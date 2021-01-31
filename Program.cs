2b27878a32e7bb05e2704ffef93e6858e0bac85a
dejiobasan <46397705+dejiobasan@users.noreply.github.com>
2021-01-31 16:39:21 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Deji_s_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
